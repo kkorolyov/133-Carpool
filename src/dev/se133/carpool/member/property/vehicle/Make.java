69334c3ffb6079826cfc43b3148768d86ecb7f48
mark-bragg <mark.bragg90@gmail.com>
2016-03-04 15:04:17 -0800

    Add car makes

--- src/dev/se133/carpool/member/property/vehicle/Make.java
+++ src/dev/se133/carpool/member/property/vehicle/Make.java
@@ -5,10 +5,77 @@ package dev.se133.carpool.member.property.vehicle;
  */
 @SuppressWarnings("javadoc")
 public enum Make {
-	TOYOTA("Toyota"),
+	ABARTH("Abarth"),
+	ALFAROMEO("Alfa Romeo"),
+	ASTONMARTIN("Aston Martin"),
+	AUSTIN("Austin"),
+	AUDI("Audi"),
+	
+	BEDFORD("Bedford"),
+	BENTLEY("Bentley"),
+	BMW("BMW"),
+	BOLWELL("Bolwell"),
+	BUFORI("Bufori"),
+	
+	CADILLAC("Cadillac"),
+	CATERHAM("Caterham"),
+	CHERY("Chery"),
+	CHEVROLET("Chevrolet"),
+	CHRYSLER("Chrysler"),
+	CITROEN("Citroen"),
+	
+	DAEWOO("Daewoo"),
+	DAIHATSU("Daihatsu"),
+	DODGE("Dodge"),
+	
+	FIAT("Fiat"),
+	FORD("Ford"),
+	
+	GEELY("Geely"),
+	GREATWALL("Great Wall"),
+	
+	HINO("Hino"),
 	HONDA("Honda"),
+	HUMMER("Hummer"),
+	HYUNDAI("Hyundai"),
+	
+	INFINITI("Infiniti"),
+	ISUZU("Isuzu"),
+	JAGUAR("Jaguar"),
+	JEEP("Jeep"),
+	
+	KIA("KIA"),
+	
+	LANDROVER("Land Rover"),
 	LEXUS("Lexus"),
-	FORD("Ford");
+	LOTUS("Lotus"),
+	
+	MAZDA("Mazda"),
+	MERCEDESBENZ("Mercedes-Benz"),
+	MINI("Mini"),
+	MITSUBISHI("Mitsubishi"),
+	
+	NISSAN("Nissan"),
+	
+	OPEL("Opel"),
+	
+	PORSCHE("Porsche"),
+	PROTON("Proton"),
+	
+	RANGEROVER("Range Rover"),
+	RENAULT("Renault"),
+	
+	SAAB("Saab"),
+	SKODA("Skoda"),
+	SSANGYONG("Ssangyong"),
+	SUBARU("Subaru"),
+	SUZUKI("Suzuki"),
+	
+	TAT("Tata"),
+	TOYOTA("Toyota"),
+	
+	VOLKSWAGEN("Volkswagen"),
+	VOLVO("Volvo");
 	
 	private String name;
 	
