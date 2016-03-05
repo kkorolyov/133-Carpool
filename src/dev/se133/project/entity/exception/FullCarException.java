bac14b35ffdab07654a8d2b831aefb761b0807b2
mark-bragg <mark.bragg90@gmail.com>
2016-03-04 20:28:54 -0800

    add to car

---
+++ src/dev/se133/project/entity/exception/FullCarException.java
@@ -0,0 +1,21 @@
+package dev.se133.project.entity.exception;
+
+public class FullCarException extends Exception{
+	//TODO Serial version UID, IDK what that is
+
+	/**
+	 * Constructs a custom instance of this exception.
+	 * @param invalidTime invalid input time
+	 * @param minTime minimum valid time
+	 * @param maxTime maximum valid time
+	 */
+	public FullCarException(int numberOfSeats) {
+		super(buildMessage(numberOfSeats));
+	}
+	
+	private static String buildMessage(int numberOfSeats) {
+		String message = "This vehicle has reached it's maximum capacity of " + numberOfSeats + " number of seats.";
+		
+		return message;
+	}
+}
