37c4400fd6ffed3eba4bc7d747719856114c22f7
IamKenshin <ryantruong1993@gmail.com>
2016-03-07 14:59:45 -0800

    Update HashAddresMap

--- src/dev/se133/project/map/HashAddressMap.java
+++ src/dev/se133/project/map/HashAddressMap.java
@@ -7,27 +7,36 @@ import dev.se133.project.commute.Address;
 
 public class HashAddressMap implements AddressMap {
 
-private  HashMap<Address, Double > commuteMap = new HashMap<>();
+	private  HashMap<Address, double[] > commuteMap;
+	private double[] coord;
 	
-	public HashAddressMap(Address departure) {
-		commuteMap.put(departure, 0.0);
+	public HashAddressMap() {
+		commuteMap = new HashMap<>();
 	}
 	
-	public HashMap<Address, Double> getCommuteMap() {
+	public HashMap<Address, double[]> getCommuteMap() {
 		return commuteMap;
 	}
 	/** Adds a new entry to the commuteMap hash. */
-	private void addCommuteDistance(Address address, double distance) {
-		commuteMap.put(address, distance);
+	private void addAddress(Address address, double x, double y) {
+		coord = new double[2];
+		coord[0] = x;
+		coord[1] = y;
+		commuteMap.put(address, coord);
 	}
 	
-	private void removeCommuteDistance(Address address) {
+	private void removeAddress(Address address) {
 		commuteMap.remove(address);
 	}
 	@Override
 	public double getDistance(Address address1, Address address2) {
+		double xDist, yDist;
+		
+		xDist = Math.abs(commuteMap.get(address1)[0] - commuteMap.get(address2)[0]);
+		yDist = Math.abs(commuteMap.get(address1)[1] - commuteMap.get(address2)[1]);
+		
 		// TODO Auto-generated method stub
-		return 0;
+		return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 	}
 
 	@Override
@@ -35,5 +44,5 @@ private  HashMap<Address, Double > commuteMap = new HashMap<>();
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
 }
