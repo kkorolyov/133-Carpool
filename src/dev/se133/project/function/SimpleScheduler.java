21b7eb9214214fd463e91a2bbe7537ba5b0e113a
IamKenshin <ryantruong1993@gmail.com>
2016-03-07 17:12:44 -0800

    Added scheduleByDriver method to simpleScheduler
    Also, changed AddressMap to a final

--- src/dev/se133/project/function/SimpleScheduler.java
+++ src/dev/se133/project/function/SimpleScheduler.java
@@ -1,6 +1,7 @@
 package dev.se133.project.function;
 
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 import dev.se133.project.commute.*;
@@ -25,7 +26,7 @@ public class SimpleScheduler {
 		this.arrival = arrival;
 	}
 	
-	public void scheduleByDistance(AddressMap map, Car car, CommutePoint departure, CommutePoint arrival) {
+	public void scheduleByDistance(final AddressMap map, Car car, CommutePoint departure, CommutePoint arrival) {
 		schedule(map, car, departure, arrival, new CommuteBuilder() {
 			@Override
 			public Commute buildCommute(Car car, CommutePoint departure, CommutePoint arrival) {
@@ -62,6 +63,54 @@ public class SimpleScheduler {
 				return commute;
 			}
 		});
+	}
+	
+	public void scheduleByDriver(final AddressMap map, Car car, CommutePoint departure, CommutePoint arrival) {
+		schedule(map, car, departure, arrival, new CommuteBuilder() {
+			public Commute buildCommute(Car car, CommutePoint departure, CommutePoint arrival) {
+				Commute commute = new Commute(departure.getDay());
+				
+				try {
+					commute.addStop(departure);	// Add departure point to commute
+					commute.addStop(arrival);	// Add arrival point to commute
+					
+					Set<Member> allInhabitants = car.getInhabitants();	// Returned set is a copy, ok to mutate
+					
+					if (car.getDriver().getAddress().equals(departure.getAddress()))	// Driver departing from own address, no need to add
+						allInhabitants.remove(car.getDriver());
+					
+					CommutePoint lastStop = departure;	// Start routing from 1st point = departure
+					
+					int id;
+					Scanner scan = new Scanner(System.in);
+					while (!allInhabitants.isEmpty()) {
+						System.out.println("Who would you like to pick up next?" + allInhabitants.iterator());
+						id = scan.nextInt();
+						double minDistance = Double.MAX_VALUE;	// No distance in commute should be greater than this
+						Member picked = null;
+						for (Member inhabitant : allInhabitants) {
+							double currentDistance = map.getDistance(lastStop.getAddress(), inhabitant.getAddress());
+							if (id == inhabitant.getId()) {
+								
+								picked = inhabitant;
+							}
+							if (currentDistance < minDistance) {
+								minDistance = currentDistance;
+							}
+						}
+						commute.addStop(lastStop = new CommutePoint(picked.getAddress(), lastStop.getDay(), new Time(lastStop.getTime().getTotalMinutes() + (int) minDistance + 1)));
+						allInhabitants.remove(picked);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				return commute;
+			}
+		});
+	}
+	
+	private void scheduleDriver() {
+		scheduleByDriver(map, car, departure, arrival);
 	}
 	
 	private void schedule(AddressMap map, Car car, CommutePoint departure, CommutePoint arrival, CommuteBuilder algorithm) {
