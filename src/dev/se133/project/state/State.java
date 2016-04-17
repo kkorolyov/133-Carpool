a22707a5eb53a41a62934486ac29eb232d3e5de9
kkorolyov <kirillk753@gmail.com>
2016-04-17 12:12:56 -0700

    Remove State's id attribute
    Seems like a useless attribute.

--- src/dev/se133/project/state/State.java
+++ src/dev/se133/project/state/State.java
@@ -4,9 +4,6 @@ package dev.se133.project.state;
  * General interface for a state.
  */
 public interface State {
-	/** @return state-specific id */
-	int getStateId();
-	
 	/** @return state-specific name */
 	String getStateName();
 }
